486535089443ce56dcc4323c43910f3c7bd5e530
NeMo <nehanrehanmohammad@gmail.com>
2026-09-09 16:33:25 +0530

    commit palindrom#3

--- 09Sep26_lab/palindrome.c
+++ 09Sep26_lab/palindrome.c
@@ -3,19 +3,14 @@
 
 #include <stdio.h>
 
-int main() {
-    char str[100];
-
-    // Read string input safely
-    printf("Input: ");
-    scanf(" %99[^\n]", str);
-
+void palindrome(const char str[]) {
     // Task 1: Find the length of the string
     int len = 0;
     while (str[len] != '\0') {
         len++;
     }
 
+    // Task 2: Check Palindrome condition
     int i;
     for (i = 0; i < len / 2; i++) {
         if (str[i] != str[len - 1 - i]) {
@@ -23,13 +18,24 @@ int main() {
         }
     }
 
+    // Task 3: Print result
     if (i == len / 2) {
         printf("Output: Palindrome\n");
     } else {
         printf("Output: Not a Palindrome\n");
     }
+}
+
+int main() {
+    char str[100];
+
+    // Read string input safely
+    printf("Input: ");
+    scanf(" %99[^\n]", str);
+
+    // Call the function
+    palindrome(str);
 
     return 0;
 }
-
 
