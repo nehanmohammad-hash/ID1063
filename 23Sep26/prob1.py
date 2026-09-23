df16fafb7c332cbd813f5f143f4d3dd1e0895c37
NeMo <nehanrehanmohammad@gmail.com>
2026-09-23 15:21:04 +0530

    commit

--- 23Sep26/prob1.py
+++ 23Sep26/prob1.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+'''
 def rms(a):
     # Convert input to a numpy array just in case it's a standard list
     arr = np.array(a, dtype=float)
@@ -7,14 +8,38 @@ def rms(a):
     # RMS formula: sqrt( mean( x^2 ) )
     result = np.sqrt(np.mean(arr ** 2))
     return result
+'''
 
+def rms_norm(arr):
+    n = len(arr)
+    # Magnitude of the vector divided by the root of dimensions
+    return np.linalg.norm(arr,2) / np.sqrt(n)
+
+
+
+'''
 inp = input("Enter numbers: ")
 tkn = inp.split()
 numbers_list = []
 for i in tkn:
     numbers_list.append(float(i))
+'''
 
-arr = np.array(numbers_list)
-ans = rms(arr)
+data = np.array([3,4,0,5])
+data1 = np.array([1, -1, 1, -1, 1])
+data2 = np.array([7.5])
+
+arr = np.array(data)
+ans = rms_norm(arr)
+print("array:",arr)
+print("rms:", ans)
+
+arr = np.array(data1)
+ans = rms_norm(arr)
+print("array:",arr)
+print("rms:", ans)
+
+arr = np.array(data2)
+ans = rms_norm(arr)
 print("array:",arr)
 print("rms:", ans)
